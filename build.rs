78be643e45128c0e59761d134aec99c264abf22c
mtantaoui <mohamedalmahdi.tantaoui@gmail.com>
2024-05-04 21:54:13 +0200

    remoe Xcompiler flag

--- build.rs
+++ build.rs
@@ -6,19 +6,19 @@ fn main() {
         .cpp(true)
         .flag("-cudart=shared")
         .flag("--use_fast_math")
-        .flag("-Xcompiler")
         .flag("-lcublas")
         .flag("-lcublasLt")
         .files(&[
             "cuda/matmul_forward/matmul_forward_ffi.cpp",
             "cuda/matmul_forward/matmul_forward.cu",
         ])
-        .compile("matmul.out");
+        .compile("kernels.out");
 
     println!("cargo:rustc-env=LD_LIBRARY_PATH=/usr/local/cuda-12.3/lib64");
 
     println!("cargo:rustc-link-search=native=/usr/local/cuda-12.3/lib64");
     println!("cargo:rustc-link-search=/usr/local/cuda-12.3/lib64");
+
     println!("cargo:rustc-link-lib=dylib=cudart");
     println!("cargo:rustc-link-lib=dylib=cublas");
     println!("cargo:rustc-link-lib=dylib=cublasLt");
